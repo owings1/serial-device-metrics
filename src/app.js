73e24e6bd4974e4e0ab533b5f137f8dd1e1dd24f
Doug Owings <doug@lab5.co>
2021-04-08 13:36:32 -0700

    serialport impl

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ const YAML  = require('yaml')
 
 const {resolve} = path
 
+const Delimiter   = require('@serialport/parser-delimiter')
+const MockBinding = require('@serialport/binding-mock')
+const SerPortFull = require('serialport')
+const SerPortMock = require('@serialport/stream')
+
+SerPortMock.Binding = MockBinding
+
 const Defaults = {
     config: {
         metrics : {},
@@ -24,6 +31,7 @@ const Defaults = {
         }
     }
 }
+
 class App {
 
     static defaults(env) {
@@ -32,8 +40,8 @@ class App {
         return {
             configFile,
             port  : +env.HTTP_PORT || 8080,
-            quiet : false,
-            mock  : false
+            quiet : !!env.QUIET,
+            mock  : !!env.MOCK
         }
     }
 
@@ -41,16 +49,54 @@ class App {
         env = env || process.env
         this.opts = merge({}, App.defaults(env), opts)
         this.httpServer = this.createServer((req, res) => this.serve(req, res))
+        this.registry = new prom.Registry
     }
 
     async start() {
+        prom.collectDefaultMetrics({register: this.registry})
         await this.loadConfig()
+        //Object.entries(this.devices).forEach(([deviceName, device]) => {
+        //    
+        //})
+
+        for (var deviceName in this.devices) {
+            await new Promise((resolve, reject) => {
+                const name = deviceName
+                const device = this.devices[name]
+                const parser = this.parsers[name]
+                this.log('Opening device', name)
+                device.open(err => {
+                    if (err) {
+                        reject(err)
+                        return
+                    }
+                    device.pipe(parser)
+                    parser.on('data', data => {
+                        try {
+                            const res = this.readDeviceData(name, data)
+                            if (res) {
+                                this.setMetricValue(name, res.metricName, res.value)
+                            }
+                        } catch (err) {
+                            this.error(err)
+                        }
+                    })
+                    resolve()
+                })
+            })
+        }
+
         this.httpServer.listen(this.opts.port)
         this.log('Listening on', this.httpServer.address())
     }
 
     async close() {
         this.httpServer.close()
+        for (var deviceName in this.devices) {
+            //this.parsers[deviceName].close()
+            this.devices[deviceName].close()
+        }
+        this.registry.clear()        
     }
 
     createServer(handler) {
@@ -73,9 +119,45 @@ class App {
         this.registry.metrics().then(metrics => res.writeHead(200).end(metrics))
     }
 
+    readDeviceData(deviceName, data) {
+
+        const parser = this.config.devices[deviceName].parser
+
+        if (data.indexOf(parser.recordStart) !== 0) {
+            throw new ParseError('Invalid start character')
+        }
+
+        const valueStartPos = data.indexOf(parser.valueStart)
+
+        if (valueStartPos < 0) {
+            throw new ParseError('Missing value start character 0x' + parser.valueStart.toString(16))
+        }
+
+        const metricName = data.slice(1, valueStartPos).toString('utf-8').trim()
+
+        if (!this.metrics[metricName]) {
+            this.log('Skipping unregistered metric', metricName)
+            return
+        }
+
+        const valueStr = data.slice(valueStartPos + 1).toString('utf-8').trim()
+        const value = parseFloat(valueStr)
+        
+        if (isNaN(value)) {
+            throw new ParseError('Cannot parse number', valueStr)
+        }
+
+        return {deviceName, metricName, value}
+    }
+
     setMetricValue(deviceName, metricName, value) {
         const labels = this.buildLabels(deviceName, metricName)
         this.metrics[metricName].labels(labels).set(value)
+        this.values[deviceName][metricName] = value
+    }
+
+    getMetricValue(deviceName, metricName) {
+        return this.values[deviceName][metricName]
     }
 
     buildLabels(deviceName, metricName) {
@@ -88,20 +170,21 @@ class App {
     }
 
     async loadConfig() {
-        const handle = await fs.open(this.opts.configFile)
-        try {
-            const conf = YAML.parse(await handle.readFile('utf-8'))
-            this.config = merge({}, Defaults.config, conf)
-        } finally {
-            handle.close()
-        }
 
-        var deviceLabels = {}
+        this.config = merge({}, Defaults.config, await this.readYamlFile(this.opts.configFile))
+
+        this.devices = {}
+        this.parsers = {}
+        this.values = {}
+        const deviceLabels = {}
         for (var deviceName in this.config.devices) {
             this.config.devices[deviceName] = merge.recursive(
                 {}, Defaults.device, this.config.devices[deviceName]
             )
             var device = this.config.devices[deviceName]
+            if (!device.path) {
+                throw new ConfigError('Missing device path for ' + deviceName)
+            }
             merge(deviceLabels, device.labels)
             for (var key of ['recordStart', 'valueStart', 'recordEnd']) {
                 device.parser[key] = parseInt(device.parser[key])
@@ -109,11 +192,11 @@ class App {
                     throw new ConfigError('Invalid integer value for ' + key)
                 }
             }
-            
+            this.devices[deviceName] = this.createDevice(device)
+            this.parsers[deviceName] = new Delimiter({delimiter: [device.parser.recordEnd]})
+            this.values[deviceName] = {}
+            this.log('Created device', deviceName, 'at', device.path)
         }
-
-        this.registry = new prom.Registry
-        prom.collectDefaultMetrics({register: this.registry})
 
         this.metrics = {}
         for (var metricName in this.config.metrics) {
@@ -125,6 +208,24 @@ class App {
                 labelNames : Object.keys(labels),
                 registers  : [this.registry]
             })
+        }
+    }
+
+    createDevice(device) {
+        var SerialPort = SerPortFull
+        if (this.opts.mock) {
+            var SerialPort = SerPortMock
+            MockBinding.createPort(device.path, {echo: true, readyData: []})
+        }
+        return new SerialPort(device.path, {baudRate: device.baudRate, autoOpen: false})
+    }
+
+    async readYamlFile(file) {
+        const handle = await fs.open(file)
+        try {
+            return YAML.parse(await handle.readFile('utf-8'))
+        } finally {
+            handle.close()
         }
     }
 
@@ -147,5 +248,6 @@ class BaseError extends Error {
 }
 
 class ConfigError extends BaseError {}
+class ParseError extends BaseError {}
 
 module.exports = App
--- test/test.js
+++ test/test.js
@@ -20,32 +20,136 @@ function newApp(configFile, opts, env) {
     return app
 }
 
+function getError(cb) {
+    try {
+        cb()
+    } catch (err) {
+        return err
+    }
+}
+
 describe('App', () => {
+
     describe('01-config.yaml', () => {
+
         var app
         var appUrl
         var metricsUrl
+
         beforeEach(async () => {
             app = newApp('01-config.yaml')
             await app.start()
             appUrl = 'http://localhost:' + app.httpServer.address().port
             metricsUrl = appUrl + '/metrics'
         })
+
         afterEach(async () => {
             await app.close()
         })
+
         it('should serve metrics', async () => {
             const res = await fetch(metricsUrl)
             const body = await res.text()
             expect(res.status).to.equal(200)
             expect(body).to.contain('process_cpu_seconds_total')
         })
-        it('should set test_metric value for device test-device to 20 and serve', async () => {
-            app.setMetricValue('test-device', 'test_metric', 20)
-            const res = await fetch(metricsUrl)
-            const body = await res.text()
-            expect(body).to.contain('test_metric')
-            expect(body).to.contain(' 20')
+
+        describe('#setMetricValue', () => {
+            it('should set test_metric value for device test-device to 20 and serve', async () => {
+                app.setMetricValue('test-device', 'test_metric', 20)
+                const res = await fetch(metricsUrl)
+                const body = await res.text()
+                expect(body).to.contain('test_metric')
+                expect(body).to.contain(' 20')
+            })
+        })
+
+        describe('#readDeviceData', () => {
+
+            it('should read test_metric=50 from buffer', () => {
+                const data = Buffer.concat([
+                    Buffer.from([0x02]),
+                    Buffer.from('test_metric'),
+                    Buffer.from([0x20]),
+                    Buffer.from('50'),
+                    Buffer.from([0x0a])
+                ])
+                const res = app.readDeviceData('test-device', data)
+                expect(res.deviceName).to.equal('test-device')
+                expect(res.metricName).to.equal('test_metric')
+                expect(res.value).to.equal(50)
+            })
+
+            it('should fail with missing recordStart', () => {
+                const data = Buffer.concat([
+                    //Buffer.from([0x02]),
+                    Buffer.from('test_metric'),
+                    Buffer.from([0x20]),
+                    Buffer.from('50'),
+                    Buffer.from([0x0a])
+                ])
+                const err = getError(() => app.readDeviceData('test-device', data))
+                expect(err.name).to.equal('ParseError')
+            })
+
+            it('should fail with missing valueStart', () => {
+                const data = Buffer.concat([
+                    Buffer.from([0x02]),
+                    Buffer.from('test_metric'),
+                    //Buffer.from([0x20]),
+                    Buffer.from('50'),
+                    Buffer.from([0x0a])
+                ])
+                const err = getError(() => app.readDeviceData('test-device', data))
+                expect(err.name).to.equal('ParseError')
+            })
+
+            it('should fail with bad value', () => {
+                const data = Buffer.concat([
+                    Buffer.from([0x02]),
+                    Buffer.from('test_metric'),
+                    Buffer.from([0x20]),
+                    Buffer.from('blargh50'),
+                    Buffer.from([0x0a])
+                ])
+                const err = getError(() => app.readDeviceData('test-device', data))
+                expect(err.name).to.equal('ParseError')
+            })
+
+            it('should return empty for unregistered metric', () => {
+                const data = Buffer.concat([
+                    Buffer.from([0x02]),
+                    Buffer.from('test_metric_non_existent'),
+                    Buffer.from([0x20]),
+                    Buffer.from('50'),
+                    Buffer.from([0x0a])
+                ])
+                const res = app.readDeviceData('test-device', data)
+                expect(!!res).to.equal(false)
+            })
+        })
+
+        describe('device#write', () => {
+            it('should set test_metric to 30 for test-device', async () => {
+                const data = Buffer.concat([
+                    Buffer.from([0x02]),
+                    Buffer.from('test_metric'),
+                    Buffer.from([0x20]),
+                    Buffer.from('30'),
+                    Buffer.from([0x0a])
+                ])
+                await new Promise((resolve, reject) => {
+                    app.devices['test-device'].write(data, err => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve()
+                        }
+                    })
+                })
+                await new Promise(resolve => setTimeout(resolve))
+                expect(app.getMetricValue('test-device', 'test_metric')).to.equal(30)
+            })
         })
     })
 })
