ccbfc330325d2b3f8d14bb72303a507a3df5a7c0
Doug Owings <doug@lab5.co>
2021-04-08 15:51:16 -0700

    parse labels registered in metric labelNames

--- src/app.js
+++ src/app.js
@@ -1,3 +1,28 @@
+/**
+ * serial-device-metrics
+ *
+ * Copyright (c) 2021 Doug Owings
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * @author Doug Owings <doug@dougowings.net>
+ */
 const fs    = require('fs').promises
 const http  = require('http')
 const merge = require('merge')
@@ -19,6 +44,9 @@ const Defaults = {
         metrics : {},
         labels  : {},
         devices : {}
+    },
+    metric: {
+        labelNames: []
     },
     device: {
         type     : 'serial',
@@ -55,10 +83,6 @@ class App {
     async start() {
         prom.collectDefaultMetrics({register: this.registry})
         await this.loadConfig()
-        //Object.entries(this.devices).forEach(([deviceName, device]) => {
-        //    
-        //})
-
         for (var deviceName in this.devices) {
             await new Promise((resolve, reject) => {
                 const name = deviceName
@@ -75,7 +99,7 @@ class App {
                         try {
                             const res = this.readDeviceData(name, data)
                             if (res) {
-                                this.setMetricValue(name, res.metricName, res.value)
+                                this.setMetricValue(name, res.metricName, res.value, res.labels)
                             }
                         } catch (err) {
                             this.error(err)
@@ -93,7 +117,6 @@ class App {
     async close() {
         this.httpServer.close()
         for (var deviceName in this.devices) {
-            //this.parsers[deviceName].close()
             this.devices[deviceName].close()
         }
         this.registry.clear()        
@@ -133,7 +156,8 @@ class App {
             throw new ParseError('Missing value start character 0x' + parser.valueStart.toString(16))
         }
 
-        const metricName = data.slice(1, valueStartPos).toString('utf-8').trim()
+        const metricStr = data.slice(1, valueStartPos).toString('utf-8').trim()
+        const {metricName, labels} = App.parseLabels(metricStr)
 
         if (!this.metrics[metricName]) {
             this.log('Skipping unregistered metric', metricName)
@@ -147,35 +171,43 @@ class App {
             throw new ParseError('Cannot parse number', valueStr)
         }
 
-        return {deviceName, metricName, value}
+        return {deviceName, metricName, value, labels}
     }
 
-    setMetricValue(deviceName, metricName, value) {
-        const labels = this.buildLabels(deviceName, metricName)
+    setMetricValue(deviceName, metricName, value, labels) {
+        labels = this.buildLabels(deviceName, metricName, labels)
         this.metrics[metricName].labels(labels).set(value)
-        this.values[deviceName][metricName] = value
+        this.lastValues[deviceName][metricName] = {value, labels}
     }
 
     getMetricValue(deviceName, metricName) {
-        return this.values[deviceName][metricName]
+        return this.lastValues[deviceName][metricName]
     }
 
-    buildLabels(deviceName, metricName) {
+    buildLabels(deviceName, metricName, labels) {
+        const registeredLabels = {}
+        
+        for (var labelName of this.config.metrics[metricName].labelNames) {
+            if (labelName in labels) {
+                registeredLabels[labelName] = labels[labelName]
+            }
+        }
         return merge(
+            registeredLabels,
             {device: deviceName},
             this.config.labels,
-            this.metrics[metricName].labels,
+            this.config.metrics[metricName].labels,
             this.config.devices[deviceName].labels
         )
     }
 
     async loadConfig() {
 
-        this.config = merge({}, Defaults.config, await this.readYamlFile(this.opts.configFile))
+        this.config = merge({}, Defaults.config, await App.readYamlFile(this.opts.configFile))
 
         this.devices = {}
         this.parsers = {}
-        this.values = {}
+        this.lastValues = {}
         const deviceLabels = {}
         for (var deviceName in this.config.devices) {
             this.config.devices[deviceName] = merge.recursive(
@@ -194,14 +226,20 @@ class App {
             }
             this.devices[deviceName] = this.createDevice(device)
             this.parsers[deviceName] = new Delimiter({delimiter: [device.parser.recordEnd]})
-            this.values[deviceName] = {}
+            this.lastValues[deviceName] = {}
             this.log('Created device', deviceName, 'at', device.path)
         }
 
         this.metrics = {}
         for (var metricName in this.config.metrics) {
+            this.config.metrics[metricName] = merge(
+                {}, Defaults.metric, this.config.metrics[metricName]
+            )
             var metric = this.config.metrics[metricName]
             var labels = merge({device: true}, this.config.labels, metric.labels, deviceLabels)
+            for (var labelName of metric.labelNames) {
+                labels[labelName] = true
+            }
             this.metrics[metricName] = new prom.Gauge({
                 name       : metricName,
                 help       : metric.help,
@@ -220,15 +258,6 @@ class App {
         return new SerialPort(device.path, {baudRate: device.baudRate, autoOpen: false})
     }
 
-    async readYamlFile(file) {
-        const handle = await fs.open(file)
-        try {
-            return YAML.parse(await handle.readFile('utf-8'))
-        } finally {
-            handle.close()
-        }
-    }
-
     log(...args) {
         if (!this.opts.quiet) {
             console.log(new Date, ...args)
@@ -237,6 +266,79 @@ class App {
 
     error(...args) {
         console.error(new Date, ...args)
+    }
+
+    static async readYamlFile(file) {
+        const handle = await fs.open(file)
+        try {
+            return YAML.parse(await handle.readFile('utf-8'))
+        } finally {
+            handle.close()
+        }
+    }
+
+    static parseLabels(input) {
+
+        const res = {labels: {}}
+
+        const openIdx = input.indexOf('{')
+
+        if (openIdx < 0) {
+            res.metricName = input.trim()
+            return res
+        }
+
+        if (input[input.length - 1] != '}') {
+            throw new ParseError('Unterminated label expression')
+        }
+
+        res.metricName = input.substring(0, openIdx).trim()
+        res.labelsStr = input.substring(openIdx + 1, input.length - 1)
+
+        var str = res.labelsStr
+
+        while (str.length) {
+
+            var eqIdx = str.indexOf('=')
+
+            if (eqIdx < 1) {
+                throw new ParseError('Missing or unexpected = in label expression')
+            }
+
+            var labelName = str.substring(0, eqIdx).trim()
+            str = str.substring(eqIdx + 1).trim()
+
+            var quoteChar = str[0]
+            if (quoteChar != '"' && quoteChar != "'") {
+                throw new ParseError('Missing open quote for label value')
+            }
+            str = str.substring(1)
+
+            var value = ''
+            var valueEndIdx = 0
+            while (str[valueEndIdx] != quoteChar) {
+
+                if (valueEndIdx > str.length - 1) {
+                    throw new ParseError('Missing close quote for label value')
+                }
+
+                if (str[valueEndIdx] == '\\') {
+                    valueEndIdx += 1
+                }
+
+                value += str[valueEndIdx]
+                valueEndIdx += 1
+            }
+
+            res.labels[labelName] = value
+            str = str.substring(valueEndIdx + 1).trim()
+
+            if (str[0] == ',') {
+                str = str.substring(1).trim()
+            }
+        }
+
+        return res
     }
 }
 
--- test/test.js
+++ test/test.js
@@ -30,6 +30,87 @@ function getError(cb) {
 
 describe('App', () => {
 
+    describe('#parseLabels', () => {
+
+        it('should parse metric name only', () => {
+            const input = 'test_metric'
+            const res = App.parseLabels(input)
+            expect(res.metricName).to.equal('test_metric')
+            expect(Object.keys(res.labels)).to.have.length((0))
+        })
+
+        it('should parse metric name and labelsStr', () => {
+            const input = 'test_metric{foo="bar"}'
+            const res = App.parseLabels(input)
+            expect(res.metricName).to.equal('test_metric')
+            expect(res.labelsStr).to.equal('foo="bar"')
+        })
+
+        it('should parse label foo="bar"', () => {
+            const input = 'test_metric{foo="bar"}'
+            const res = App.parseLabels(input)
+            expect(res.labels.foo).to.equal('bar')
+        })
+
+        it('should throw for missing end brace', () => {
+            const input = 'test_metric{foo="bar"'
+            const err = getError(() => App.parseLabels(input))
+            expect(err.name).to.equal('ParseError')
+        })
+
+        it('should throw for missing equals', () => {
+            const input = 'test_metric{foo"bar"}'
+            const err = getError(() => App.parseLabels(input))
+            expect(err.name).to.equal('ParseError')
+        })
+
+        it('should throw for missing label name', () => {
+            const input = 'test_metric{="bar"}'
+            const err = getError(() => App.parseLabels(input))
+            expect(err.name).to.equal('ParseError')
+        })
+
+        it('should throw for missing open quote', () => {
+            const input = 'test_metric{foo=bar}'
+            const err = getError(() => App.parseLabels(input))
+            expect(err.name).to.equal('ParseError')
+        })
+
+        it('should throw for missing close quote', () => {
+            const input = 'test_metric{foo="bar}'
+            const err = getError(() => App.parseLabels(input))
+            expect(err.name).to.equal('ParseError')
+        })
+
+        it('should parse escaped quotes', () => {
+            const input = 'test_metric{foo="\\\"value\\\""}'
+            const res = App.parseLabels(input)
+            expect(res.labels.foo).to.equal('"value"')
+        })
+
+        it('should parse label1="value1" and label2="value2"', () => {
+            const input = 'test_metric{label1="value1",label2="value2"}'
+            const res = App.parseLabels(input)
+            expect(res.labels.label1).to.equal('value1')
+            expect(res.labels.label2).to.equal('value2')
+        })
+
+        it("should parse single quotes label1='value1' and label2='value2'", () => {
+            const input = "test_metric{label1='value1',label2='value2'}"
+            const res = App.parseLabels(input)
+            expect(res.labels.label1).to.equal('value1')
+            expect(res.labels.label2).to.equal('value2')
+        })
+
+        it('should ignore spaces', () => {
+            const input = 'test_metric { label1 = "value1"  , label2  = "value2" }'
+            const res = App.parseLabels(input)
+            expect(res.metricName).to.equal('test_metric')
+            expect(res.labels.label1).to.equal('value1')
+            expect(res.labels.label2).to.equal('value2')
+        })
+    })
+
     describe('01-config.yaml', () => {
 
         var app
@@ -148,7 +229,116 @@ describe('App', () => {
                     })
                 })
                 await new Promise(resolve => setTimeout(resolve))
-                expect(app.getMetricValue('test-device', 'test_metric')).to.equal(30)
+                expect(app.getMetricValue('test-device', 'test_metric').value).to.equal(30)
+            })
+        })
+    })
+
+    describe('02-config.yaml', () => {
+
+        var app
+        var appUrl
+        var metricsUrl
+
+        beforeEach(async () => {
+            app = newApp('02-config.yaml')
+            await app.start()
+            appUrl = 'http://localhost:' + app.httpServer.address().port
+            metricsUrl = appUrl + '/metrics'
+        })
+
+        afterEach(async () => {
+            await app.close()
+        })
+
+        it('should write test_metric=6 to test-device-1 and =8 to test-device-2', async () => {
+            const getData = value => Buffer.concat([
+                Buffer.from([0x02]),
+                Buffer.from('test_metric'),
+                Buffer.from([0x20]),
+                Buffer.from(value.toString()),
+                Buffer.from([0x0a])
+            ])
+            await new Promise((resolve, reject) => {
+                app.devices['test-device-1'].write(getData(6), err => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+                })
+            })
+            await new Promise((resolve, reject) => {
+                app.devices['test-device-2'].write(getData(8), err => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve()
+                    }
+                })
+            })
+            await new Promise(resolve => setTimeout(resolve))
+            expect(app.getMetricValue('test-device-1', 'test_metric').value).to.equal(6)
+            expect(app.getMetricValue('test-device-2', 'test_metric').value).to.equal(8)
+        })
+    })
+    
+    describe('03-config.yaml', () => {
+
+        var app
+        var appUrl
+        var metricsUrl
+
+        beforeEach(async () => {
+            app = newApp('03-config.yaml')
+            await app.start()
+            appUrl = 'http://localhost:' + app.httpServer.address().port
+            metricsUrl = appUrl + '/metrics'
+        })
+
+        afterEach(async () => {
+            await app.close()
+        })
+
+        function getData(metricStr, value) {
+            return Buffer.concat([
+                Buffer.from([0x02]),
+                Buffer.from(metricStr),
+                Buffer.from([0x20]),
+                Buffer.from(value.toString()),
+                Buffer.from([0x0a])
+            ])
+        }
+        describe('#readDeviceData', () => {
+            it('should read labels', async () => {
+                const data = getData('test_metric{label_name_test="test-value"}', '50')
+                const res = app.readDeviceData('test-device', data)
+                expect(res.labels.label_name_test).to.equal('test-value')
+            })
+        })
+
+        describe('#setMetricValue', () => {
+            it('should remove unregistered label', () => {
+                app.setMetricValue('test-device', 'test_metric', 80, {label_name_test: 'value', unreg_label: 'value'})
+                expect(Object.keys(app.getMetricValue('test-device', 'test_metric').labels)).to.not.contain('unreg_label')
+            })
+        })
+
+        describe('device#write', () => {
+            it('should set test_metric to 30 for test-device', async () => {
+                const data = getData('test_metric{label_name_test="test-value"}', '30')
+                await new Promise((resolve, reject) => {
+                    app.devices['test-device'].write(data, err => {
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve()
+                        }
+                    })
+                })
+                await new Promise(resolve => setTimeout(resolve))
+                expect(app.getMetricValue('test-device', 'test_metric').value).to.equal(30)
+                expect(app.getMetricValue('test-device', 'test_metric').labels.label_name_test).to.equal('test-value')
             })
         })
     })
